0083503222d9113cb3a0af90fc552e60567109e4
MohamedMujtaba <65574561+MohamedMujtaba@users.noreply.github.com>
2020-09-20 20:35:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// document.getElementById("button").addEventListener("click", hi);
+
+// function hi() {
+//   // let colleg = document.getElementById("selecter-one").value;
+//   let year = document.getElementById("selecter-two").value;
+//   //   let sims = document.getElementById("selecter-three").value;
+//   dhy(year);
+// }
+// function dhy() {
+//   if (year === 0) {
+//     console.log(hi);
+//   } else {
+//   }
+// }
